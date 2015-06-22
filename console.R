fade81cebcebece8a3b299e00cf6b3f2cb0cb3c8
Joseph Slone <joe.slone@gmail.com>
2015-06-22 16:07:48 -0400

    created the switch statement

--- console.R
+++ console.R
@@ -8,3 +8,38 @@ m <- min(outcome[, 11], na.rm = TRUE)
 is equal to the scalr "m"
 
 s <- subset(outcome, outcome[, 11] == m)
+
+###
+#  There are 3 columns of data to look at besides the name and state of the
+#  hospitals.
+#
+#   UF Name             #   Name
+#
+#  "heart attack"       11
+#  "heart failure"      17
+#  "pneumonia"          23
+#
+
+# First a working example.
+
+test1 <- function(type) {
+    switch(type,
+           mean = 1,
+           median = 2,
+           trimmed = 3)
+}
+
+# Now, let's test my function
+
+colID <- function(name) {
+
+    switch(tolower(name),
+           "hear attack" = 11,
+           "heart failure" = 17,
+           "pneumonia" = 23,
+           0)
+}
+
+Function to trim white space from a string.
+
+function(x) gsub("^[[:space:]]+|[[:space:]]+$", "", x)
